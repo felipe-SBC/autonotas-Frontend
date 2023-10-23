bc39a19789f100d60ec1fa20885afdeba05558b4
Luis Felipe <luis.felipe.lima23@gmail.com>
2023-10-23 16:48:44 -0300

    :sparkles: colocando componente Paper

--- src/components/Professor/createProfessor/index.js
+++ src/components/Professor/createProfessor/index.js
@@ -1,4 +1,4 @@
-import { TextField, Grid, Button, Typography } from "@mui/material";
+import { TextField, Grid, Button, Typography, Paper } from "@mui/material";
 import React, { useState } from "react";
 
 const CreateProfessor = () => {
@@ -30,22 +30,34 @@ const CreateProfessor = () => {
   return (
     <div>
       <form onSubmit={handleSubmit}>
-        <Typography variant="h5">Fazer Cadastro</Typography>
-        <Grid container spacing={2}>
-          <Grid item xs={12}>
-            <TextField id="nome" name="nome" label="Nome" value={professor.nome} onChange={handleChange} variant="outlined"/>
+        <Paper 
+            elevation={2}
+            sx={{
+              backgroundColor: '#F7F9F9', paddingTop: '36px', paddingBottom: '36px', borderRadius: '10px', width: '400px',
+              textAlign: 'center'
+            }}
+          >
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <Typography variant="h5">Fazer Cadastro</Typography>
+            </Grid>
+            <Grid item xs={12}>
+              <TextField id="nome" name="nome" label="Nome" value={professor.nome} onChange={handleChange} variant="outlined"/>
+            </Grid>
+            <Grid item xs={12}>
+              <TextField id="telefone" name="telefone" label="Telefone" value={professor.telefone} onChange={handleChange} variant="outlined"/>
+            </Grid>
+            <Grid item xs={12}>
+              <TextField id="email" name="email" type="email" label="Email" value={professor.email} onChange={handleChange} variant="outlined"/>
+            </Grid>
+            <Grid item xs={12}>
+              <TextField id="senha" type="password" name="senha" label="Senha" value={professor.senha} onChange={handleChange} variant="outlined"/>
+            </Grid>
+            <Grid item xs={12}>
+              <Button type="submit" variant="contained">Enviar</Button>
+            </Grid>
           </Grid>
-          <Grid item xs={12}>
-            <TextField id="telefone" name="telefone" label="Telefone" value={professor.telefone} onChange={handleChange} variant="outlined"/>
-          </Grid>
-          <Grid item xs={12}>
-            <TextField id="email" name="email" type="email" label="Email" value={professor.email} onChange={handleChange} variant="outlined"/>
-          </Grid>
-          <Grid item xs={12}>
-            <TextField id="senha" type="password" name="senha" label="Senha" value={professor.senha} onChange={handleChange} variant="outlined"/>
-          </Grid>
-        </Grid>
-        <Button type="submit">Enviar</Button>
+        </Paper>
       </form>
     </div>
   );
